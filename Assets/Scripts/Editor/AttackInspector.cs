4eca34b1ca7b9a0bd45d91aadb4c30371c70f637
Balint720 <ovalentin8@gmail.com>
2026-02-05 20:23:49 +0100

    Got inspector working for animations

--- Assets/Scripts/Editor/AttackInspector.cs
+++ Assets/Scripts/Editor/AttackInspector.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using Codice.CM.WorkspaceServer.Lock;
+using NUnit.Framework.Internal;
+using Unity.Properties;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEditor.UIElements;
@@ -11,10 +14,25 @@ using UnityEngine.UIElements;
 [CustomEditor(typeof(AttackClass))]
 public class AttackInspector : Editor
 {
-    public VisualTreeAsset m_AttackInspectorUXML;
+    const float paddingLeftLength = 50.0f;
+    const float h2 = 16.0f;
+
+    SerializedObject sObj;
+
+    SerializedProperty hurtboxesProperty;
+    SerializedProperty animationSizeProperty;
+    SerializedProperty animationProperty;
+
+
     public override VisualElement CreateInspectorGUI()
     {
+        // Get the target object
         AttackClass at = (AttackClass)target;
+
+        sObj = new SerializedObject(at);
+        
+        // Properties
+        hurtboxesProperty = sObj.FindProperty("hurtboxes");
 
         // Create a new VisualElement to be the root of our Inspector UI.
         VisualElement myInspector = new VisualElement();
@@ -22,52 +40,115 @@ public class AttackInspector : Editor
         // Add a simple label.
         myInspector.Add(new Label("This is a custom Inspector"));
 
+        /*
         // Load the UXML file and clone its tree into the inspector
         if (m_AttackInspectorUXML != null)
         {
             VisualElement uxmlContent = m_AttackInspectorUXML.CloneTree();
             myInspector.Add(uxmlContent);
         }
+        */
 
-        // Hurtbox field
-        ObjectField hurtbox = new ObjectField();
-        hurtbox.label = "Get hurtboxes from GameObject children";
-        hurtbox.bindingPath = "";
-        hurtbox.objectType = typeof(GameObject);
+        // Box for base stats of attack
+        Box statBox = new Box();
 
-        // Hurtbox strings
+        Label statBoxLabel = new Label("Base stats");
+        statBoxLabel.style.fontSize = h2;
 
-        List<String> hurtboxNames = at.hurtboxes;
-
-        ListView hurtboxNameList = new ListView();
-        hurtboxNameList.makeItem = () =>  new TextField();
-        hurtboxNameList.bindItem = (e, i) =>
+        // Damage and knockback fields
+        IntegerField damage = new IntegerField()
         {
-            ((TextField)e).value = hurtboxNames[i];
-            ((TextField)e).style.paddingLeft = new StyleLength(100);
+            label = "Damage of attack",
+            bindingPath = "damage",
+        };
+        FloatField knockback = new FloatField()
+        {
+            label = "Knockback value of attack",
+            bindingPath = "knockback"
         };
 
-        hurtboxNameList.itemsSource = hurtboxNames;
-        hurtboxNameList.bindingPath = "hurtboxes";
+        damage.style.paddingLeft = new StyleLength(paddingLeftLength);
+        knockback.style.paddingLeft = new StyleLength(paddingLeftLength);
 
-        hurtbox.RegisterValueChangedCallback(
+        // Adding fields to box
+        statBox.Add(statBoxLabel);
+        statBox.Add(damage);
+        statBox.Add(knockback);
+
+        // Hurtbox box
+        Box hurtboxBox = new Box();
+
+        Label hurtboxLabel = new Label("Hurtboxes");
+        hurtboxLabel.style.fontSize = h2;
+
+        // Field for gathering hurtboxes from GameObject
+        // This is a convenience field so we do not have to write every single hurtbox, we can load them up
+        ObjectField hurtboxObject = new ObjectField()
+        {
+            label = "Get hurtboxes from GameObject and its children",
+            bindingPath = "",
+            objectType = typeof(GameObject)
+        };
+
+        // Item count for listing index of items
+        int indexCountHurtbox = 0;
+        //int indexCountAnim = 0;
+
+        // Hurtbox strings which will be saved to AttackClass object
+        if (at.hurtboxes == null) at.hurtboxes = new List<String>();
+        List<String> hurtboxNames = at.hurtboxes;
+        ListView hurtboxNameList = new ListView()
+        {
+            // Every item is a new textfield
+            makeItem = () =>  new TextField() 
+                {
+                    label = (indexCountHurtbox++).ToString()
+                },
+
+            // Binding items
+            bindItem = (e, i) =>
+            {
+                while (hurtboxesProperty.arraySize <= i)
+                {
+                    hurtboxesProperty.arraySize++;
+                }
+                ((TextField)e).BindProperty(hurtboxesProperty.GetArrayElementAtIndex(i));
+                ((TextField)e).style.paddingLeft = new StyleLength(paddingLeftLength);
+            },
+            itemsSource = hurtboxNames,
+            bindingPath = "hurtboxes",
+
+            // Show + and - icon, allow them to be pressed
+            showAddRemoveFooter = true,
+            allowAdd = true,
+            allowRemove = true,
+
+            // Allow reordering of items in list
+            reorderable = true,
+        };
+
+        // On object loaded into hurtboxObject, gather all hurtboxes from object
+        hurtboxObject.RegisterValueChangedCallback(
             (evt) =>
             {
-                Transform[] cols = ((GameObject)hurtbox.value).GetComponentsInChildren<Transform>();
+                Transform[] cols = ((GameObject)hurtboxObject.value).GetComponentsInChildren<Transform>();
                 
                 foreach (Transform c in cols)
                 {
-                    if (c.name.Contains("Hurtbox") || c.name.Contains("HurtBox"))
+                    if (c.name.Contains("Hurtbox") || c.name.Contains("HurtBox") || c.name.Contains("hurtbox"))
                     {
                         hurtboxNames.Add(c.name);
                     }
                 }
 
+                // Refresh items, because we added new ones
                 hurtboxNameList.RefreshItems();
             }
 
         );
-
+        
+        // Event on pressing the "+" icon
+        // Adds an empty string to end of list
         hurtboxNameList.onAdd += (i) => 
         {
             hurtboxNames.Add("");
@@ -75,6 +156,8 @@ public class AttackInspector : Editor
             hurtboxNameList.RefreshItems();
         };
 
+        // Event on pressing the "-" icon
+        // Removes either selected item OR the last item (if selection is incorrect)
         hurtboxNameList.onRemove += (i) => 
         {
             if (i.selectedIndex >= 0 && i.selectedIndex < hurtboxNames.Count)
@@ -87,15 +170,63 @@ public class AttackInspector : Editor
             }
         };
 
-        hurtboxNameList.showAddRemoveFooter = true;
-        hurtboxNameList.allowAdd = true;
-        hurtboxNameList.allowRemove = true;
-        hurtboxNameList.reorderable = true;
+        // Adding fields to box
+        hurtboxBox.Add(hurtboxLabel);
+        hurtboxBox.Add(hurtboxObject);
+        hurtboxBox.Add(hurtboxNameList);
 
-        myInspector.Add(hurtbox);
-        myInspector.Add(hurtboxNameList);
+        
+        // Box for animations
+        Box animBox = new Box();
+        Label animLabel = new Label("Animations");
+        animLabel.style.fontSize = h2;
+
+        ListView animList = new ListView()
+        {
+            bindingPath = "animations",
+            showAddRemoveFooter = true,
+            allowAdd = true,
+            allowRemove = true,
+
+            virtualizationMethod = CollectionVirtualizationMethod.FixedHeight,
+            fixedItemHeight = 250.0f
+        };
+
+
+
+        
+
+        // Add fields to box
+        animBox.Add(animLabel);
+        animBox.Add(animList);
+
+        // Add the elements to inspector object
+        myInspector.Add(statBox);
+        myInspector.Add(hurtboxBox);
+        myInspector.Add(animBox);
 
         // Return the finished Inspector UI.
         return myInspector;
+    }
+
+    public ListView CreateListView()
+    {
+        var listView = new ListView()
+        {
+            virtualizationMethod = CollectionVirtualizationMethod.FixedHeight,
+            showFoldoutHeader = true,
+            headerTitle = "Elements",
+            makeItem = () => {
+                var propertyField = new PropertyField();
+                propertyField.bindingPath = "myField";
+
+                var container = new BindableElement(); // This MUST be a bindable element!
+                container.Add(propertyField);
+                return container;
+            }
+        };
+        
+        return listView;
+        // NOTE: Don't forget to add the list view to your tree!
     }
 }
--- Assets/Scripts/Enemy/Attack/AttackClass.cs
+++ Assets/Scripts/Enemy/Attack/AttackClass.cs
@@ -1,6 +1,82 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
+
+// Wrapper classes
+[System.Serializable]
+public class ListWrapper<T> : IList<T>
+{
+    public List<T> innerList;
+
+    public int Count => innerList.Count;
+    public int Capacity => innerList.Capacity;
+
+    public bool IsReadOnly => ((ICollection<T>)innerList).IsReadOnly;
+
+    public ListWrapper()
+    {
+        innerList = new List<T>();
+    }
+
+    public T this[int key]
+    {
+        get { return innerList[key]; }
+        set {innerList[key] = value; }
+    }
+
+    public void Add(T value)
+    {
+        innerList.Add(value);
+    }
+
+    public void RemoveAt(int index)
+    {
+        innerList.RemoveAt(index);
+    }
+
+    public int IndexOf(T item)
+    {
+        return innerList.IndexOf(item);
+    }
+
+    public void Insert(int index, T item)
+    {
+        innerList.Insert(index, item);
+    }
+
+    public void Clear()
+    {
+        ((ICollection<T>)innerList).Clear();
+    }
+
+    public bool Contains(T item)
+    {
+        return ((ICollection<T>)innerList).Contains(item);
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        ((ICollection<T>)innerList).CopyTo(array, arrayIndex);
+    }
+
+    public bool Remove(T item)
+    {
+        return ((ICollection<T>)innerList).Remove(item);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return ((IEnumerable<T>)innerList).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return ((IEnumerable)innerList).GetEnumerator();
+    }
+}
+
 
 // Use the CreateAssetMenu attribute to allow creating instances of this ScriptableObject from the Unity Editor.
 [CreateAssetMenu(fileName = "Attack", menuName = "ScriptableObjects/Attack", order = 1)]
@@ -9,7 +85,8 @@ public class AttackClass : ScriptableObject
     public int damage;
     public float knockback;
     public List<string> hurtboxes;
-    public List<Animation[]> animations;
-    public List<int[]> activeInactiveFrames;
+    [SerializeField] public List<ListWrapper<AnimationClip>> animations;
+    public AnimationClip testAnimation;
+    public List<ListWrapper<int>> activeInactiveFrames;
 
 }
